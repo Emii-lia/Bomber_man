2da6556ad458f10712319ca0192c44f2b94c15e8
Emii-lia <fiaromiangaly@gmail.com>
2024-02-18 12:55:23 +0300

    add: initiate bomber_man

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
     <header className="relative overflow-hidden font-mono h-screen">
         <Navbar/>
         <div className="relative z-10 w-3/5 p-8 my-56 ml-32 text-white">
+          <h4 className="mt-6 font-bold text-7xl">Explosion!</h4>
           <h2 className="font-extrabold text-7xl">Boom! </h2>
-          <h4 className="mt-6 font-bold text-7xl">Explosion</h4>
           <p className="mt-6 mb-10 text-lg font-semibold leading-loose">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
             quia hic dolorum iste maxime nisi et nesciunt eligendi amet
